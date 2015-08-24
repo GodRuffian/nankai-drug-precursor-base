031b97734698ff8b8adc1a50bef09ca9a085c35f
冷凯 <lengkai@YangWangtekiMacBook-Pro-2.local>
2015-08-24 16:52:40 +0800

    采购轮次表

---
+++ class/Gini/ORM/Version.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Gini\ORM;
+
+class Version extends Object
+{
+    public $group = 'object:group';
+
+    // 状态
+    public $status = 'int,default:0';
+
+    // 开始时间
+    public $start_time = 'datetime';
+    //结束时间
+    public $end_time = 'datetime';
+
+    public $ctime = 'datetime';
+    public $mtime = 'datetime';
+
+    // 关闭
+    const STATUS_OFF = 0;
+    // 开启
+    const STATUS_PENDING = 1;
+}
