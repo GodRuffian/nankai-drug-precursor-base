125f337048ed5bb11566774e912e36fc56a0e5ee
pihizi <pihizi@msn.com>
2015-08-25 10:19:02 +0800

    database.yml检测

--- class/Gini/Module/NankaiDrugPrecursorBase.php
+++ class/Gini/Module/NankaiDrugPrecursorBase.php
@@ -7,4 +7,21 @@ class NankaiDrugPrecursorBase
     public static function setup()
     {
     }
+
+    public static function diagnose()
+    {
+        $error = [];
+        // 1. check database: SHOW TABLES?
+        $db = \Gini\Database::db();
+        $ret = $db->query('SHOW TABLES');
+        if (!$ret) {
+            $error[] = 'Please config your database in raw/config/@'
+                .($_SERVER['GINI_ENV'] ?: 'production').'/database.yml!';
+        }
+
+        if (!empty($error)) {
+            return $error;
+        }
+
+    }
 }
