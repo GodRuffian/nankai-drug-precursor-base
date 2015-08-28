0d8807ffbbfd5fa9566c60ed7a23aab794064116
pihizi <pihizi@msn.com>
2015-08-28 13:42:07 +0800

    plan/product & plan/order

---
+++ class/Gini/ORM/Plan/Order.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Gini\ORM\Plan;
+
+class Order extends \Gini\ORM\Object
+{
+    // 计划信息
+    public $plan = 'object:plan';
+
+    public $order_id = 'bigint';
+}
+
--- class/Gini/ORM/Plan/Product.php
+++ class/Gini/ORM/Plan/Product.php
@@ -1,14 +1,17 @@
 <?php
 
-namespace Gini\ORM;
+namespace Gini\ORM\Plan;
 
-class Product extends Object
+class Product extends \Gini\ORM\Object
 {
     // 计划信息
     public $plan = 'object:plan';
 
     public $product_id = 'bigint';
     public $product_name = 'string:255';
+    public $product_version = 'int';
+    public $product_quantity = 'int';
+    public $product_package = 'string:50';
 
     public $vendor_id = 'bigint';
     public $vendor_name = 'string:150';
