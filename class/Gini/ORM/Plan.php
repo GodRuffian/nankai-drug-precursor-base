593648619c3ddd1eb1d989b70612ace2c4900cfc
pihizi <pihizi@msn.com>
2015-08-31 12:51:31 +0800

    orm/plan提供方法获取plan关联的订单是否发生了变化

--- class/Gini/ORM/Plan.php
+++ class/Gini/ORM/Plan.php
@@ -49,4 +49,83 @@ class Plan extends Object
             self::STATUS_REJECTED => T('被驳回')
         ];
     }
+
+    public function getNewInfo()
+    {
+        $products = (array)$this->getRemoteProducts();
+        $info = (array)$this->info;
+        $data = [];
+        $hasErr = false;
+        if (!empty($info)) {
+            foreach ($info as $pid=>$value) {
+                if (isset($products[$pid])) {
+                    if (!$hasErr && $value['total']!=$products[$pid]['total']) {
+                        $hasErr = true;
+                    }
+                    $data[$pid] = [
+                        'name'=> $products[$pid]['name'],
+                        'cas_no'=> $products[$pid]['cas_no'],
+                        'values'=> $value['values'],
+                        'total'=> $products[$pid]['total'],
+                        'orders'=> $value['orders'],
+                        // 商品信息的源数据
+                        'raw'=> $value['raw'],
+                    ];
+                }
+            }
+            if (!$hasErr && !empty(array_diff(array_keys($info), array_keys($products)))) {
+                $hasErr = true;
+            }
+        }
+        foreach (array_diff(array_keys($products), array_keys($data)) as $pid) {
+            $data[$pid] = $products[$pid];
+        }
+        return [$hasErr, $data];
+    }
+
+    public function getRemoteProducts()
+    {
+        static $result;
+        if ($result) return $result;
+        $confs = \Gini\Config::get('mall.rpc');
+        $conf = $confs['default'] ?: [];
+        try {
+            $rpc = \Gini\IoC::construct('\Gini\RPC', $conf['url']);
+            $client = \Gini\Config::get('mall.client');
+            $token = $rpc->mall->authorize($client['id'], $client['secret']);
+            if (!$token) return;
+            $round = those('round')->orderBy('id', 'desc')->limit(1)->current();
+            if (!$round->id) return;
+            $data = (array)$rpc->mall->order->getDrugPrecursorProducts(_G('GROUP')->id, $round->ctime, date('Y-m-d H:i:s'));
+            $tmp = [];
+            foreach ($data as $id=>$value) {
+                $tmp[$value['cas_no']]['ids'][] = $id;
+                $tmp[$value['cas_no']]['name'] = array_unique(array_merge((array)$tmp[$value['cas_no']]['name'], (array)$value['name']));
+                // TODO 单位换算
+                $tmp[$value['cas_no']]['total'] += $value['quantity'];
+                $tmp[$value['cas_no']]['orders'] = array_unique(array_merge((array)$tmp[$value['cas_no']]['orders'], (array)$value['orders']));
+                // 商品信息的源数据
+                $tmp[$value['cas_no']]['raw'][$id] = [
+                    'name'=> $value['name'],
+                    'quantity'=> $value['quantity'],
+                    'package'=> $value['package'],
+                    'vendor_id'=> $value['vendor_id'],
+                    'vendor_name'=> $value['vendor_name'],
+                ];
+            }
+            foreach ($tmp as $casNO=>$value) {
+                $result[implode(',', $value['ids'])] = [
+                    'name'=> implode(',', $value['name']),
+                    'cas_no'=> $casNO,
+                    'total'=> $value['total'],
+                    'orders'=> $value['orders'],
+                    // 商品信息的源数据
+                    'raw'=> $value['raw'],
+                ];
+            }
+        }
+        catch(\Exception $e) {
+        }
+        return $result ?: [];
+    }
 }
