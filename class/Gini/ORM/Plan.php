320387858c042c2101b3292d74d2e0bf4b922218
Jinlin Li <jinlin.li@geneegroup.com>
2015-08-31 14:35:43 +0800

    获取远程商品信息的时候，获得规格信息

--- class/Gini/ORM/Plan.php
+++ class/Gini/ORM/Plan.php
@@ -109,6 +109,7 @@ class Plan extends Object
                     'name'=> $value['name'],
                     'quantity'=> $value['quantity'],
                     'package'=> $value['package'],
+                    'spec' => $values['spec'],
                     'vendor_id'=> $value['vendor_id'],
                     'vendor_name'=> $value['vendor_name'],
                 ];
