9ad55af54b0e7b8a67bc6ec86e088acd0b21bdc5
pihizi <pihizi@msn.com>
2015-09-02 13:51:30 +0800

    加入rpc配置的检测

--- class/Gini/Module/NankaiDrugPrecursorBase.php
+++ class/Gini/Module/NankaiDrugPrecursorBase.php
@@ -19,6 +19,19 @@ class NankaiDrugPrecursorBase
                 .($_SERVER['GINI_ENV'] ?: 'production').'/database.yml!';
         }
 
+        $confs = \Gini\Config::get('mall.rpc');
+        $conf = $confs['default'] ?: [];
+        if (empty($conf)) {
+            $error[] = '请配置mall.rpc.default';
+        }
+        else {
+            $rpc = \Gini\IoC::construct('\Gini\RPC', $conf['url']);
+            $client = \Gini\Config::get('mall.client');
+            if (!$rpc->mall->authorize($client['id'], $client['secret'])) {
+                $error[] = '请配置mall.rpc配置存在异常，无法通过authorize';
+            }
+        }
+
         if (!empty($error)) {
             return $error;
         }
